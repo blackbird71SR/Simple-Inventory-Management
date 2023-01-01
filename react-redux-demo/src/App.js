e9ebad459e9f964190f26f92042ccfa28ffc0055
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-01-01 17:24:28 -0600

    Add new component for iceCream and update accessing store due to reducer splitting

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,19 +1,21 @@
 import { Provider } from 'react-redux';
 
 import './App.css';
-import CakeContainer from './components/CakeContainer';
+// import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
+import HooksIceCreamContainer from './components/HooksIceCreamContainer';
 import store from './redux/store';
 
 function App() {
   return (
     <Provider store={store}>
       <div className='App'>
-        <h3>Without Hooks</h3>
-        <CakeContainer />
-        <hr />
-        <h3>With Hooks</h3>
+        {/* <h3>Without Hooks</h3>
+        <CakeContainer /> */}
+        {/* <h3>With Hooks</h3> */}
         <HooksCakeContainer />
+        <hr />
+        <HooksIceCreamContainer />
       </div>
     </Provider>
   );
---
+++ react-redux-demo/src/components/HooksIceCreamContainer.js
@@ -0,0 +1,16 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { buyIceCream } from '../redux';
+
+const HooksIceCreamContainer = () => {
+  const numOfIceCreams = useSelector((state) => state.iceCream.numOfIceCreams);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>No. of IceCreams - {numOfIceCreams}</h2>
+      <button onClick={() => dispatch(buyIceCream())}>Buy IceCream</button>
+    </div>
+  );
+};
+
+export default HooksIceCreamContainer;
