0032530b37272dd94c2a37a1229c86415babf355
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-01-01 14:20:09 -0600

    Add alternative approach of hhoks for accessing store and dispatching action

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -2,13 +2,18 @@ import { Provider } from 'react-redux';
 
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 import store from './redux/store';
 
 function App() {
   return (
     <Provider store={store}>
       <div className='App'>
+        <h3>Without Hooks</h3>
         <CakeContainer />
+        <hr />
+        <h3>With Hooks</h3>
+        <HooksCakeContainer />
       </div>
     </Provider>
   );
